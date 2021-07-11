ebfbb807bc1c9a4041246ed479e6718b3eb08c1e
vijaymannava <86002106+vijaymannava@users.noreply.github.com>
2021-07-11 15:26:25 +0530

    Modifications done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import uvicorn
+from scipy.sparse import issparse
 from fastapi import FastAPI
 from pydantic import BaseModel
 from ml_utils import load_model, predict
+from datetime import datetime, timezone
 
 app = FastAPI(
     title="Iris Predictor",
@@ -16,21 +18,40 @@ class QueryIn(BaseModel):
     petal_length: float
     petal_width: float
 
-class QueryOut(BaseModel):
+class FeedbackIn(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
     flower_class: str
 
+class QueryOut(BaseModel):
+    flower_class: str
 
 @app.get("/ping")
 def ping():
     return {"ping": "pong"}
 
+#@app.post("/feedback_loop", status_code=200)
+#def feedback_loop(
+#        data: list[FeedbackIn]
+#):
+#    retrain(data)
+#    return {"detail": "Feedback loop successful"}
+
+
+@app.get("/Time")
+async def root(start_date: datetime = datetime.now(timezone.utc)):
+    print(start_date)
+    return {"Time_Stamp": start_date}
 
 @app.post("/predict_flower", response_model=QueryOut, status_code=200)
 def predict_flower(
     query_data: QueryIn
-):
+  ):
     output = {'flower_class': predict(query_data)}
     return output
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=8888, reload=True)
